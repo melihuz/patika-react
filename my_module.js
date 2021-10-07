7114d36384aaca4482eedd7839c50733136e1f2f
melihuz <36965784+melihuz@users.noreply.github.com>
2021-10-07 15:19:59 +0300

    function update on module

--- my_module.js
+++ my_module.js
@@ -1,15 +1,5 @@
 import axios from "axios";
 
-// export default async (number) => {
-//   const { data: user } = await axios(
-//     "https://jsonplaceholder.typicode.com/users/" + number
-//   );
-//   const { data: posts } = await axios(
-//     "https://jsonplaceholder.typicode.com/posts?userId=" + number
-//   );
-//   user.posts = posts;
-//   return user;
-// };
 
 const getData = (number) => {
   return new Promise(async (resolve, reject) => {
